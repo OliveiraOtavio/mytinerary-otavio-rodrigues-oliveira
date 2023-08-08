e6f73dcc904014db511ecf76419dd85e3cc81971
Otavio <otavio.oliveira@msn.com>
2023-08-08 08:34:34 -0300

    effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Carrousel from "../components/Carrousel";
+import axios from "axios";
+
 
 export default function Home() {
   const [show, setShow] = useState(true);
+  const [data, setData] = useState([]);
 
-  let data = [
-    { id: "am1", city: "Cancun", photo: "/img/america/cancun.jpg" },
-    { id: "am2", city: "New York", photo: "/img/america/newyork.jpg" },
-    { id: "am3", city: "Rio de Janeiro", photo: "/img/america/rioDeJaneiro.jpg"},
-    { id: "am4", city: "Ushuaia", photo: "/img/america/ushuaia.jpg" },
-    { id: "as1", city: "Bangkok", photo: "/img/asia/bangkok.jpg" },
-    { id: "as2", city: "Pekin", photo: "/img/asia/pekin.jpg" },
-    { id: "as3", city: "Singapur", photo: "/img/asia/singapur.jpg" },
-    { id: "as4", city: "Tokyo", photo: "/img/asia/tokio.jpg" },
-    { id: "eu1", city: "Ibiza", photo: "/img/europe/ibiza.jpg" },
-    { id: "eu2", city: "London", photo: "/img/europe/london.jpg" },
-    { id: "eu3", city: "Paris", photo: "/img/europe/paris.jpg" },
-    { id: "eu4", city: "Roma", photo: "/img/europe/roma.jpg" },
-    { id: "oc1", city: "Majuro", photo: "/img/oceania/majuro.jpg" },
-    { id: "oc2", city: "Sidney", photo: "/img/oceania/sidney.jpg" },
-    { id: "oc3", city: "Suva", photo: "/img/oceania/suva.jpg" },
-    { id: "oc4", city: "Wellington", photo: "/img/oceania/wellington.jpg" },
-  ];
+
+  useEffect( 
+    ()=>{
+      axios('/data.json')
+        .then(res=> setData(res.data))
+        .catch(err=> console.log(err))
+    },
+    []
+  )
 
   return (
     <main className="flex items-top flex-col sm:flex-row w-[95vw] mt-20 border-2 border-blue-500 ">
@@ -54,10 +48,8 @@ export default function Home() {
         </button>
       </div>
 
-      {/* CARROUSEL */} 
-      
+      {/* CARROUSEL */}       
       {/* ELIMINAR BOTON SHOW/HIDE */}
-
       {show ? (
         <input
           onClick={() => setShow(!show)}
@@ -75,7 +67,8 @@ export default function Home() {
       )}
 
       {show ? <Carrousel data={data} /> : <h1 className="flex items-center my-auto mx-5 bg-blue-300 h-max p-3 rounded-[5px]"> Click to see cards! </h1>}
-      {/* <Carrousel data={data} /> */}
+      {/* <Carrousel data={data} /> */} 
+
     </main>
   );
 }
